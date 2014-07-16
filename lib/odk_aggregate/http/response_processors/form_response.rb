4345d3aea09d6958370ae648c66a63a9b00ab3ec
noam <noam@clusterfoo.com>
2014-07-16 19:22:34 -0400

    card 36 -- fix odk stuff

--- lib/odk_aggregate/http/response_processors/form_response.rb
+++ lib/odk_aggregate/http/response_processors/form_response.rb
@@ -1,8 +1,14 @@
+require 'uri'
+
 module OdkAggregate
   class FormResponse
 
-    def initialize(response)
+    def initialize(response, connection, username, password)
       @response = response
+      @connection = connection
+      @username = username
+      @password = password
+      @download_url = download_url
     end
 
     def full_response
@@ -10,7 +16,8 @@ module OdkAggregate
     end
 
     def download_url
-      @response["downloadUrl"]
+      url = URI(@response["downloadUrl"])
+      url.path.to_s + "?" +  url.query.to_s
     end
 
     def manifest_url
@@ -22,12 +29,14 @@ module OdkAggregate
     end
 
     def fields
-      response = MultiXml.parse Faraday.new(download_url).get.body.gsub("\n", ""), typecast_xml_value: false
-      response["html"]["head"]["model"]["bind"]
+      #response = MultiXml.parse Faraday.new(download_url).get.body.gsub("\n", ""), typecast_xml_value: false
+      #response = MultiXml.parse @connection.send(:get, @download_url).body.gsub("\n", ""), typecast_xml_value: false
+      response = @connection.send(:get, @download_url)
+      response.body["html"]["head"]["model"]["bind"]
     end
 
     def get_top_element
-      response = MultiXml.parse Faraday.new(download_url).get.body.gsub("\n", ""), typecast_xml_value: false
+      response = MultiXml.parse Faraday.new(@download_url).get.body.gsub("\n", ""), typecast_xml_value: false
       response["html"]["head"]["model"]["bind"].first["nodeset"].split("/")[1]
     end
   end
