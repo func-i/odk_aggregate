4c7534d0f82aa47c346b890ae558fa8e9218bfd9
noam <noam@clusterfoo.com>
2014-07-28 18:02:13 -0400

    FIX: some fields not being parse correctly to JSON and not showing up

--- lib/odk_aggregate/resources/form.rb
+++ lib/odk_aggregate/resources/form.rb
@@ -1,4 +1,5 @@
 require 'odk_aggregate/http/response_processors/form_response'
+require 'multi_xml'
 
 module OdkAggregate
   module Form
@@ -17,13 +18,13 @@ module OdkAggregate
     end
 
     def last_form
-      OdkAggregate::FormResponse.new @connection.send(:get, 'xformsList').body["xforms"]["xform"].last,
-                                     @connection, @username, @password
+      response = MultiXml.parse(@connection.send(:get, 'xformsList', formID: id, verbose: true).body)["xforms"]["xform"] 
+      OdkAggregate::FormResponse.new reponse.last, @connection, @username, @password
     end
 
     def find_form(id)
-      OdkAggregate::FormResponse.new @connection.send(:get, 'xformsList', formID: id, verbose: true).body["xforms"]["xform"],
-                                     @connection, @username, @password
+      response = MultiXml.parse(@connection.send(:get, 'xformsList', formID: id, verbose: true).body)["xforms"]["xform"] 
+      OdkAggregate::FormResponse.new response, @connection, @username, @password
     end
 
   end
